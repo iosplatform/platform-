56383e469b509e66a11971f1f466bfa2d1ddff11
iOS platform <108881734+iosplatform@users.noreply.github.com>
2022-07-15 02:14:20 +0600

    Hfhfhc
    Hchchc

---
+++ communit.js
@@ -0,0 +1,7 @@
+var string = "Hello! Welcome to iOS community. Start use magic thing with us together :) ";
+var str = string.split("");
+var el = document.getElementById('str');
+(function animate() {
+str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
+var running = setTimeout(animate, 90);
+})();
